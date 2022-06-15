adb9888186b1de042bc0e7400d3f0c9fe03ad456
Ser <ser.fala@gmail.com>
2022-06-15 00:12:36 -0300

    Update TpProg3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TpProg3';
+  constructor() {
+    this.zoom = 2;
+    this.lat = 0;
+    this.lng = 0;
+  }
+
+  zoom: number;
+  lat: number;
+  lng: number;
+
+  handleInputChange(event: Event) {
+    const target = <HTMLInputElement>event.target;
+    if (target) {
+      if (target.name === 'zoom') {
+        this.zoom = parseFloat(target.value);
+      }
+      if (target.name === 'lat') {
+        this.lat = parseFloat(target.value);
+      }
+      if (target.name === 'lng') {
+        this.lng = parseFloat(target.value);
+      }
+    }
+  }
 }
